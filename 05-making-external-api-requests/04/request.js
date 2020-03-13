6cf8a1adf15daedb3457eb2d40c80b4206c3ce30
Armen <avanesi.armen@gmail.com>
2020-03-14 09:05:09 +1100

    Use 'axios' instead of 'request' to make API requests - the corresponding video clip has been updated

--- 05-making-external-api-requests/04/request.js
+++ 05-making-external-api-requests/04/request.js
@@ -1,9 +1,52 @@
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 
-request('http://www.google.com', (error, response, body) => {
-  console.log('body:', body);
-}).pipe(fs.createWriteStream('google.html'));
+// GET request used:
+axios({
+  method: 'get',
+  url : 'http://www.google.com',
+  responseType: 'stream'
+})
+.then((response) => {
+  response.data.pipe(fs.createWriteStream('google.html'));
+})
+.catch((error) => {
+  console.error(error);
+});
 
-fs.createReadStream(__dirname + '/payload.json')
-.pipe(request.post('http://localhost:8080/users'));
+// POST request used:
+axios({
+  method: 'post',
+  url : 'http://localhost:8080/users',
+  data: {
+    userNames: ['dannyt100', 'freddyv100']
+  },
+  transformRequest: (data, headers) => {
+    const newData = data.userNames.map((userName) => {
+      return userName + '!';
+    });
+    return JSON.stringify(newData);
+  }
+})
+.then((response) => {
+  console.log(response);
+})
+.catch((error) => {
+  console.error(error);
+});
+
+const getMetadata = () => {
+  return axios.get('http://localhost:8080/metadata?id=1');
+};
+
+const getMetadataAgain = () => {
+  return axios.get('http://localhost:8080/metadata?id=1');
+};
+
+// Making concurrent requests:
+axios.all([
+  getMetadata(), getMetadataAgain()
+]).then((responseArray) => {
+  console.log(responseArray[0].data.description);
+  console.log(responseArray[1].data.description);
+});
